4b4da49bf3eb6596af4b54cbdfea289c5d1d6877
TJ Kandala <kandalatj@gmail.com>
2020-06-28 21:52:26 -0400

    handling cmdline args

--- nit/src/index.ts
+++ nit/src/index.ts
@@ -1,13 +1,26 @@
 /**
  * Building Git
  *
- * TODO: Configure tests. Mock the filesystem
- *
- * Don't forget to (somehow) use Worker Threads and Child Processes!
+ * TODO:
+ * - Configure tests. Mock the filesystem.
+ * - Don't forget to (somehow) use Worker Threads and Child Processes!
+ * - Include benchmarks! There's not really any context for them, however.
  */
 
 const path = process.cwd();
 
-console.log(path);
+const arg = process.argv[2];
 
-console.log(process.argv);
+if (arg == null) {
+  console.error("You didn't provide a path argument");
+  process.exit();
+}
+
+switch (arg) {
+  case "hi":
+    console.log("hi to you too!");
+    break;
+
+  default:
+    console.log(arg);
+}
