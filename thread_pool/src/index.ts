3b4b1242b5499a1e82e1cecc288b6a61ab4417a4
TJ Kandala <kandalatj@gmail.com>
2020-06-30 19:31:53 -0400

    worker calls are now async

--- thread_pool/src/index.ts
+++ thread_pool/src/index.ts
@@ -7,41 +7,54 @@ import { Worker, isMainThread, workerData, parentPort } from "worker_threads";
  * after, implement thread pooling
  */
 
-// const myWorker = new Worker(__filename, {
-//   workerData: { count: isMainThread ? 1 : workerData.count + 1 },
-// });
-
 function typeSafeWorker<T extends (...args: any) => void>(fn: T) {
+  console.log("public api called");
+
   const myWorker = new Worker(
     `
-    const {parentPort} = require("worker_threads");
-    console.log("meep");
-    ${fn.toString()}
-    parentPort?.on("message", (val) => {
-          ${fn.name}(...val.args)
-          process.exit()
-    });
-      `,
+      const {parentPort} = require("worker_threads");
+      ${fn.toString()}
+      parentPort?.on("message", (val) => {
+            ${fn.name}(...val.args);
+
+            parentPort?.postMessage("received");
+      });
+        `,
     {
       eval: true,
     }
   );
 
-  return [
-    (...args: Parameters<T>) => {
-      console.log("public api called");
-      myWorker.postMessage({
-        args,
-      });
-    },
-  ];
+  return async (...args: Parameters<T>) => {
+    myWorker.postMessage({
+      args,
+    });
+
+    return new Promise((resolve) => {
+      function cleanup(val: any) {
+        if (val === "received") {
+          resolve();
+          myWorker.off("message", cleanup);
+        }
+      }
+
+      myWorker.on("message", cleanup);
+    });
+  };
 }
 
 function addTwo(first: number, second: number) {
   console.log(first + second);
 }
 
-const [coolAddTwo] = typeSafeWorker(addTwo);
+const coolAddTwo = typeSafeWorker(addTwo);
 
-coolAddTwo(1, 2);
-coolAddTwo(2, 2);
+async function main() {
+  await coolAddTwo(1, 2);
+  await coolAddTwo(2, 2);
+
+  console.log("complete!");
+  process.exit();
+}
+
+main();
