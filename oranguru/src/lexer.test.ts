d675bed9df770275eb582bcd98676f78a2cdf337
TJ Kandala <kandalatj@gmail.com>
2020-07-06 18:44:36 -0400

    first good lexer test

--- oranguru/src/lexer.test.ts
+++ oranguru/src/lexer.test.ts
@@ -11,14 +11,52 @@ describe("lexer", () => {
     
     let result = add(five, ten);`;
 
+    const expectedTokens = [
+      ["LET", "let"],
+      ["IDENT", "five"],
+      ["ASSIGN", "="],
+      ["INT", "5"],
+      ["SEMICOLON", ";"],
+      ["LET", "let"],
+      ["IDENT", "ten"],
+      ["ASSIGN", "="],
+      ["INT", "10"],
+      ["SEMICOLON", ";"],
+      ["LET", "let"],
+      ["IDENT", "add"],
+      ["ASSIGN", "="],
+      ["FUNCTION", "fn"],
+      ["LPAREN", "("],
+      ["IDENT", "x"],
+      ["COMMA", ","],
+      ["IDENT", "y"],
+      ["RPAREN", ")"],
+      ["LBRACE", "{"],
+      ["IDENT", "x"],
+      ["PLUS", "+"],
+      ["IDENT", "y"],
+      ["SEMICOLON", ";"],
+      ["RBRACE", "}"],
+      ["LET", "let"],
+      ["IDENT", "result"],
+      ["ASSIGN", "="],
+      ["IDENT", "add"],
+      ["LPAREN", "("],
+      ["IDENT", "five"],
+      ["COMMA", ","],
+      ["IDENT", "ten"],
+      ["RPAREN", ")"],
+      ["SEMICOLON", ";"],
+    ];
+
     const tokens = [];
 
     for (let token of createLexer(program)) {
       tokens.push(token);
     }
 
-    console.log(tokens);
-
-    expect(true).toBe(true);
+    for (let i = 0; i < tokens.length; i++) {
+      expect(tokens[i]).toStrictEqual(expectedTokens[i]);
+    }
   });
 });
