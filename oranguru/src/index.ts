20887aa56e9d3ef547672a89244436bd9d39321b
TJ Kandala <kandalatj@gmail.com>
2020-07-07 14:44:07 -0400

    oranguru: lexing stdin

--- oranguru/src/index.ts
+++ oranguru/src/index.ts
@@ -1,4 +1,27 @@
+import { createInterface } from "readline";
+import { createLexer } from "./lexer";
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 /**
  * REPL
  */
-function main() {}
+function main() {
+  console.log("starting REPL");
+
+  rl.on("line", (input) => {
+    console.log(input);
+
+    const tokens = [];
+    for (const token of createLexer(input)) {
+      tokens.push(token);
+    }
+
+    console.log(tokens);
+  });
+}
+
+main();
